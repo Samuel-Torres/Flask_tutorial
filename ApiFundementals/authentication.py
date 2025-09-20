5a5df8112f8a6a1df3c93b32bd12741a8db6bbd1
Samuel Torres <rilla@MacBook-Pro.local>
2025-09-20 17:26:17 -0400

    Learned to access system env and .env files

--- ApiFundementals/authentication.py
+++ ApiFundementals/authentication.py
@@ -1,15 +1,35 @@
 """
 API authentication:
 
+To get ENV Variables:
+pip install python-dotenv
 
 """
 
+import os
 import requests
+
+# GET ENV VAR USING SYSTEM ENVIRONMENT VARIABLES:
+
+# api_key = os.getenv("owm_api_key")
+# print(api_key)
+
+# GET ENV VARS USING DOTENV:
+# install pip install python-dotenv
+
+# import dotenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key = os.getenv("owm_api_key")
+# print("API Key:", api_key)
+
 
 url = "https://api.openweathermap.org/data/2.5/weather"
 parameters = {
     "q": "Paris",
-    "appid": "6e43c75c642a9923b980f2077e880777",
+    "appid": api_key,
 }
 
 fetch = requests.get(url, params=parameters)
---
+++ ApiFundementals/environmentVariables.py
@@ -0,0 +1,13 @@
+"""
+To export variables into the env file you need to export it in the terminal command line:
+
+export name=anything
+
+To view env variables in terminal:
+bash: env --> view all variables
+
+
+use env variables:
+bash: pip install python-dotenv
+
+"""
